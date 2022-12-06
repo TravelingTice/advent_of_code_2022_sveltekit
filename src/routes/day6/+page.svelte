054275f6be438f21cade48bc70fc8521f1464172
Matthijs Kralt <matthijs@webinargeek.com>
2022-12-06 14:19:55 +0100

    Add rust to the project, complete dev environment

---
+++ src/routes/day6/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import init, { add } from 'day-6-rust'
+	import { onMount } from 'svelte'
+
+	onMount(async () => {
+		await init()
+
+		console.log(add(1, 2))
+	})
+</script>
+
+<h2>Day 6 in WebAssembly!</h2>
